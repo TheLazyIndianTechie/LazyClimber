690d5bc83de913ad9f9e5d579ba9384390ba29e5
Vinay Vidyasagar <86476092+TheLazyIndianTechie@users.noreply.github.com>
2023-10-24 16:42:40 +0530

    Set up a basic mesh creation manager to handle creation based on user input

---
+++ Assets/LazyClimber/Scripts/MeshCreationManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace LazyClimber
+{
+    public class MeshCreationManager : MonoBehaviour
+    {
+        public void BeginDraw()
+        {
+            // Start drawing from input
+        }
+    
+        public void EndDraw()
+        {
+            // Stop drawing when user releases input
+        }
+    }
+}
