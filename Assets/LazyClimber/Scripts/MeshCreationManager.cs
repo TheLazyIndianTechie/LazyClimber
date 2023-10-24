076457a413010ead8f65e7b83ed7ee104e4f4ac7
Vinay Vidyasagar <86476092+TheLazyIndianTechie@users.noreply.github.com>
2023-10-24 17:59:56 +0530

    Fixed multiple inputs with an if condition, returning when context is not performed

--- Assets/LazyClimber/Scripts/MeshCreationManager.cs
+++ Assets/LazyClimber/Scripts/MeshCreationManager.cs
@@ -13,6 +13,9 @@ namespace LazyClimber
         
         public void BeginDraw(InputAction.CallbackContext ctx)
         {
+            //Return if context is not performed. Avoid multiples
+            if (!ctx.performed) return;
+            
             // Start drawing from input
             var message = "Begin Draw: " + ctx;
             Debug.Log(message);
@@ -21,8 +24,10 @@ namespace LazyClimber
     
         public void EndDraw(InputAction.CallbackContext ctx)
         {
+            //Return if context is not performed. Avoid multiples
+            if (!ctx.performed) return;
+            
             // Stop drawing when user releases input
-            Debug.Log(ctx.ToString());
             var message = "End Draw: " + ctx;
             Debug.Log(message);
             OnEndDraw?.Invoke(message);
